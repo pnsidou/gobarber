aa1c0c33f170519dabb993afb413238bb194ab94
Pedro N. Sidou <pnsidou@gmail.com>
2020-01-30 18:05:43 -0300

    chore: configure sqlize
    Basic configuration of sql database.
    
    Configured user migration.

---
+++ server/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgresql',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}

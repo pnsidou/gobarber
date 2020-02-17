620727cc0d455208416afa7d3e45f07cb2f8d9d3
Pedro N. Sidou <pnsidou@gmail.com>
2020-02-17 16:07:41 -0300

    feat: add RouteWrapper to handle redirects
    This functionality assures that when an unauthenticated user
    tries to access a private page, he is redirected to
    the login page. On the other hand, if the user is not signed
    in and the page is not private, the user is redirected to
    the dashboard page.

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
@@ -10,11 +11,11 @@ import Profile from '../pages/Profile';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact compoment={SignIn} />
-      <Route path="/register" compoment={SignUp} />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" compoment={Dashboard} />
-      <Route path="/profile" compoment={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }

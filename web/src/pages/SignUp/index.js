9142a4827c58eb20abeed4027b0bebf0f8db5eff
Pedro N. Sidou <pnsidou@gmail.com>
2020-02-17 21:09:23 -0300

    feat: configure redux, redux-saga and reactotron-redux-saga

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Input, Form } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha necessita ter ao menos 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
-  return <div></div>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="goBarber" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+      Login
+    </>
+  );
 }

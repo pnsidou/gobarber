fb0692929c298688fe67a6945f23b8197ca0b9bf
Pedro N. Sidou <pnsidou@gmail.com>
2020-02-19 12:59:14 -0300

    feat: implement authentication mechanism

--- web/src/store/modules/auth/reducer.js
+++ web/src/store/modules/auth/reducer.js
@@ -1,7 +1,20 @@
-const INITIAL_STATE = {};
+import produce from 'immer';
+
+const INITIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
 
 export default function auth(state = INITIAL_STATE, action) {
+  console.tron.log(action);
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
+
     default:
       return state;
   }

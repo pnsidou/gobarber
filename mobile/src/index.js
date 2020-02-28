f6e0b4a5f50159699ebd2841f673c20b9a696e17
Pedro N. Sidou <pnsidou@gmail.com>
2020-02-28 14:36:41 -0300

    chore: mobile initial commit

---
+++ mobile/src/index.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+
+const App: () => React$Node = () => {
+  return <></>;
+};
+
+export default App;

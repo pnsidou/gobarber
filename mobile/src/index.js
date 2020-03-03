5c6d6e07b6467fc0af0da83a259dc0931b1f097f
Pedro N. Sidou <pnsidou@gmail.com>
2020-03-03 11:21:34 -0300

    feat: initial route

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -15,17 +15,17 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
-import Routes from './routes';
+import App from './App';
 
-const App: () => React$Node = () => {
+const AppWrapper: () => React$Node = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <Routes />
+        <App />
       </PersistGate>
     </Provider>
   );
 };
 
-export default App;
+export default AppWrapper;
